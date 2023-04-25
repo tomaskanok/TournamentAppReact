299d2111f6a4e866f42bd80d14e18b398d8e16fd
Tomas Kanok <kanoktomas@gmail.com>
2023-04-25 19:37:10 +0200

    ui

--- src/app/Fighter.tsx
+++ src/app/Fighter.tsx
@@ -1,13 +1,19 @@
-export default function Fighter(props) {
+interface Fighter {
+    name: string;
+    record: string;
+    corner: "blue" | "red";
+}
+
+export default function Fighter(props: Fighter) {
     return (
         <div className="flex">
             {
                 props.corner === 'blue' &&
-                <span className="bg-blue-600 w-2 h-6 block border border-blue-600"></span>
+                <span className="bg-blue-600 w-2 h-6 block border border-blue-600 mr-2"></span>
             }
             {
                 props.corner === 'red' &&
-                <span className="bg-red-600 w-2 h-6 block border border-red-600"></span>
+                <span className="bg-red-600 w-2 h-6 block border border-red-600 mr-2"></span>
             }
 
             <span>{props.name}, {props.record}</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,49 +8,121 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [fighters, setFighters] = useState([
     {
-      "name": "John Smith",
-      "wins": 8,
-      "losses": 2,
-      "gym": "Team Alpha Male",
+      "name": "Daniel Johnson",
+      "wins": 3,
+      "losses": 3,
+      "gym": "American Top Team",
       "weight": 93,
       "gender": "male"
     },
     {
-      "name": "Mike Johnson",
-      "wins": 6,
+      "name": "Michael Davis",
+      "wins": 10,
+      "losses": 2,
+      "gym": "Blackzilians",
+      "weight": 93,
+      "gender": "male"
+    },
+    {
+      "name": "David Rodriguez",
+      "wins": 7,
       "losses": 4,
       "gym": "Jackson Wink MMA",
+      "weight": 93,
+      "gender": "male"
+    },
+    {
+      "name": "Jason Lee",
+      "wins": 8,
+      "losses": 2,
+      "gym": "Tristar Gym",
+      "weight": 93,
+      "gender": "male"
+    },
+    {
+      "name": "Mark Thompson",
+      "wins": 9,
+      "losses": 1,
+      "gym": "Team Quest",
+      "weight": 93,
+      "gender": "male"
+    },
+    {
+      "name": "Paul Kim",
+      "wins": 5,
+      "losses": 2,
+      "gym": "Alliance MMA",
+      "weight": 93,
+      "gender": "male"
+    },
+    {
+      "name": "Robert Green",
+      "wins": 3,
+      "losses": 0,
+      "gym": "American Kickboxing Academy",
+      "weight": 93,
+      "gender": "male"
+    },
+    {
+      "name": "Sean Johnson",
+      "wins": 4,
+      "losses": 1,
+      "gym": "Nova Uniao",
       "weight": 93,
       "gender": "male"
     }
   ]);
 
   return (
-    <main className="">
-      <div className='flex justify-center divide-x'>
-        <div className='flex'>
+    <main className="pt-5">
+      <div className='grid grid-rows-1 justify-center gap-4'>
+        <div className='flex p-5 rounded-lg bg-white'>
           <Fighter
             name={fighters[0].name}
             record={fighters[0].wins + "-" + fighters[0].losses}
             corner="blue" />
-          <div className='mx-8'>Light heavyweight</div>
+          <div className='mx-8'>{fighters[0].weight} kg</div>
           <Fighter
             name={fighters[1].name}
             record={fighters[1].wins + "-" + fighters[1].losses}
             corner="red" />
-        </div>        
+        </div>   
 
-        <div className='flex'>
+        <div className='flex p-5 rounded-lg bg-white'>
           <Fighter
-            name={fighters[0].name}
-            record={fighters[0].wins + "-" + fighters[0].losses}
+            name={fighters[2].name}
+            record={fighters[2].wins + "-" + fighters[2].losses}
             corner="blue" />
-          <div className='mx-8'>Light heavyweight</div>
+          <div className='mx-8'>{fighters[2].weight} kg</div>
           <Fighter
-            name={fighters[1].name}
-            record={fighters[1].wins + "-" + fighters[1].losses}
+            name={fighters[3].name}
+            record={fighters[3].wins + "-" + fighters[3].losses}
             corner="red" />
-        </div>        
+        </div>  
+
+        <div className='flex p-5 rounded-lg bg-white'>
+          <Fighter
+            name={fighters[4].name}
+            record={fighters[4].wins + "-" + fighters[4].losses}
+            corner="blue" />
+          <div className='mx-8'>{fighters[4].weight} kg</div>
+          <Fighter
+            name={fighters[5].name}
+            record={fighters[5].wins + "-" + fighters[5].losses}
+            corner="red" />
+        </div>  
+
+        <div className='flex p-5 rounded-lg bg-white'>
+          <Fighter
+            name={fighters[6].name}
+            record={fighters[6].wins + "-" + fighters[6].losses}
+            corner="blue" />
+          <div className='mx-8'>{fighters[6].weight} kg</div>
+          <Fighter
+            name={fighters[7].name}
+            record={fighters[7].wins + "-" + fighters[7].losses}
+            corner="red" />
+        </div>       
       </div>
 
     </main>
